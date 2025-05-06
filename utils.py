24d59f0c1c2e8fb7d86e4e569d2c76f58123e0ae
Daniel <dan.pfister27@gmail.com>
2025-05-06 14:59:28 +0200

    only avoid cells near enemy head if they're longer

--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
 import typing
 
 def get_free_fields(game_state):
-    fields = set((i, j) for i in range(11) for j in range(11))
+    fields = set(
+        (i, j)
+        for i in range(game_state["board"]["width"])
+        for j in range(game_state["board"]["height"])
+    )
     moves = {"up": (0, 1), "down": (0, -1), "left": (-1, 0), "right": (1, 0)}
+    my_length = game_state["you"]["length"]
     
     for b in game_state["you"]["body"]:
         fields.discard((b["x"], b["y"]))
@@ -10,8 +15,11 @@ def get_free_fields(game_state):
     for snake in game_state["board"]["snakes"]:
         if snake["id"] == game_state["you"]["id"]: continue
         head = snake["body"][0]
-        for move in moves.values(): # ignore cells where enemy head can move
-            fields.discard((head["x"] + move[0], head["y"] + move[1]))
+        
+        if snake["length"] >= my_length: # for longer snakes discard cells where enemy head can move
+            for move in moves.values():
+                fields.discard((head["x"] + move[0], head["y"] + move[1]))
+                
         for b in snake["body"]:
             fields.discard((b["x"], b["y"]))
             
