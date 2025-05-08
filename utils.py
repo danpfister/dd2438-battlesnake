ae6224c63d1721ed0027146b7a4b7144d7ef84c6
Daniel <dan.pfister27@gmail.com>
2025-05-08 14:30:37 +0200

    tweaked score calculation

--- utils.py
+++ utils.py
@@ -29,15 +29,19 @@ def get_free_fields(game_state: dict):
 
 def get_scores(game_state: dict, food_distances: dict, floodfill_distances: dict):
     # scale values to [0-1]
+    food_distances = {k: max(0, 10 - v) for k, v in food_distances.items()}
     max_food_distance = max(max(food_distances.values()), 1)
-    food_distances = {k: max(0, 10 - v) / max_food_distance for k, v in food_distances.items()}
+    food_distances = {k: v / max_food_distance for k, v in food_distances.items()}
     
+    floodfill_distances = {k: (v - 5) for k, v in floodfill_distances.items()}
     max_floodfill_distance = max(max(floodfill_distances.values()), 1)
-    floodfill_distances = {k: (v - 5) / max_floodfill_distance for k, v in floodfill_distances.items()}
+    floodfill_distances = {k: v / max_floodfill_distance for k, v in floodfill_distances.items()}
     
-    # TODO add fixed rules or something
     scores = {}
     for move in food_distances.keys():
-        scores[move] = FOOD_WEIGHT * food_distances[move] + (1 - FOOD_WEIGHT) * floodfill_distances[move]
+        if game_state["you"]["health"] < 25: # prevents snake from dying of hunger
+            scores[move] = food_distances[move]
+        else:
+            scores[move] = FOOD_WEIGHT * food_distances[move] + (1 - FOOD_WEIGHT) * floodfill_distances[move]
     
     return scores
