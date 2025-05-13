853246943c5c3ad62a4df2d8178aa5255b88906a
Banah Abdeljaber <babdeljaber14@gmail.com>
2025-05-13 16:35:52 +0200

    added attacking but could be improved

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import typing
+import floodfill
 
 FOOD_WEIGHT = 0.7
 
@@ -35,6 +36,18 @@ def get_free_fields(game_state: dict, safe_mode=True):
             
     return fields
 
+def get_enemy_heads(game_state: dict):
+    my_id = game_state["you"]["id"]
+    return [
+        {
+            "pos": (snake["head"]["x"], snake["head"]["y"]),
+            "length": snake["length"],
+        }
+        for snake in game_state["board"]["snakes"]
+        if snake["id"] != my_id
+    ]
+
+
 def get_scores(game_state: dict, food_distances: dict, floodfill_distances: dict):
     # scale values to [0-1]
     food_distances = {k: max(0, 10 - v) for k, v in food_distances.items()}
@@ -45,12 +58,41 @@ def get_scores(game_state: dict, food_distances: dict, floodfill_distances: dict
     max_floodfill_distance = max(max(floodfill_distances.values()), 1)
     floodfill_distances = {k: v / max_floodfill_distance for k, v in floodfill_distances.items()}
     
+    my_head = (game_state["you"]["body"][0]["x"], game_state["you"]["body"][0]["y"])
+    my_length = game_state["you"]["length"]
+    enemy_heads = get_enemy_heads(game_state)
+    move_to_delta = {"up": (0, 1), "down": (0, -1), "left": (-1, 0), "right": (1, 0)}
+    
     scores = {}
     for move in food_distances.keys():
+        food_score = food_distances[move]
+        space_score = floodfill_distances[move]
+
         if game_state["you"]["health"] < 25: # prevents snake from dying of hunger
-            scores[move] = food_distances[move]
-        else:
-            scores[move] = FOOD_WEIGHT * food_distances[move] + (1 - FOOD_WEIGHT) * floodfill_distances[move]
+            score = food_score
+        score = FOOD_WEIGHT * food_score + (1 - FOOD_WEIGHT) * space_score
+
+        # add offensive bonus
+        dx, dy = move_to_delta[move]
+        new_pos = (my_head[0] + dx, my_head[1] + dy)
+
+        for enemy in enemy_heads:
+            enemy_pos = enemy["pos"]
+            dist = abs(new_pos[0] - enemy_pos[0]) + abs(new_pos[1] - enemy_pos[1])
+
+            # head-to-head opportunities or risks
+            if dist == 1:
+                if my_length > enemy["length"]:
+                    score += 0.3  
+
+            # trap potential if you control more space
+            enemy_area = floodfill.flood_fill_max_area(game_state, enemy_pos, move)
+
+            if floodfill_distances[move] > (enemy_area / 10):  
+                score += 0.2
+
+
+        scores[move] = score
     
     return scores
 
